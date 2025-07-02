f1437b32b54185c588a6af4436b8359ba40ab4c2
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-02 15:51:14 +0300

    feat: add header html and styling

--- Football-Tournament/src/components/Navigation/Navigation.jsx
+++ Football-Tournament/src/components/Navigation/Navigation.jsx
@@ -1,16 +1,23 @@
 import { NavLink } from "react-router"
 
+import styles from "./navigation.module.css"
 export const Navigation = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink
-                        to="/">
-                        Home
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+            <div className={styles.navContainer}>
+                <div className={styles.logo}>
+                    <h3>Football</h3>
+                    <h3>Championship</h3>
+                </div>
+                <nav className={styles.nav}>
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
     )
 }
