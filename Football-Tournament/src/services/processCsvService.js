b5e31e5cca0e6d6f56aa9dda9642a97dc102003a
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-02 17:39:37 +0300

    feat: implement csv process functionality
    Implement fetch csv function which returns textData from the csv file and function which parses text data to array with objects.

---
+++ Football-Tournament/src/services/processCsvService.js
@@ -0,0 +1,31 @@
+const parseCsv = (textData) => {
+    let splittedData = textData.split("\n");
+    let headers = splittedData.shift().split(',').map(currentHeader => currentHeader.trim());
+    
+    let arrayWithDataObjects = splittedData.map((currentLine) => {
+        const lineValues = currentLine.split(',').map(currentElement => currentElement.trim());
+        const tempObject = {};
+        // for each header map its corresponding value from the line !(headers and lines length is equal)!
+        // header[0] -> id : line[0] -> 1 = id: 1 
+        headers.forEach((value, index) => tempObject[value] = lineValues[index]);
+        console.log(lineValues)
+        return tempObject
+    })
+
+    return arrayWithDataObjects
+}
+
+const fetchCsv = async (filePath) => {
+	const someData = await fetch(filePath); // "/data/players.csv"
+	const textData = await someData.text();
+
+    parseCsv(textData);
+};
+
+
+export const csvFileProcessor = {
+    getPlayers: () => fetchCsv("/data/players.csv"),
+    getTeams: () => fetchCsv("/data/teams.csv"),
+    getMatches: () => fetchCsv("/data/matches.csv"),
+    getRecords: () => fetchCsv("/data/records.csv"),
+}
