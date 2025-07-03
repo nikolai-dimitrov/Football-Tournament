eae21cefd8fb14ef6ab0461c176b0057609e9c68
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-03 15:21:02 +0300

    feat: implement functionality to map teams with matches
    Find team A corresponding with ATeamID in all matches and add property ATeamName which contains team name matches object. Same operation for team B

--- Football-Tournament/src/contexts/TournamentContext.jsx
+++ Football-Tournament/src/contexts/TournamentContext.jsx
@@ -1,10 +1,42 @@
-import { createContext } from "react"
+import { useState, useEffect, createContext } from "react"
+import { csvFileProcessor } from "../services/processCsvService";
 
 export const TournamentContext = createContext();
 export const TournamentProvider = ({ children }) => {
+    const [players, setPlayers] = useState([]);
+    const [teams, setTeams] = useState([]);
+    const [records, setRecords] = useState([]);
+    const [matches, setMatches] = useState([]);
+
+    useEffect(() => {
+        Promise.all([
+            csvFileProcessor.getMatches(),
+            csvFileProcessor.getTeams(),
+        ]).then(([matchesData, teamsData]) => {
+            setMatches(matchesData);
+            setTeams(teamsData)
+        }).catch((error) => console.log(error));
+    }, [])
+
+    // Map ATeam name with ATeamID and BTeam name with BTeamID (implement reduce for better performance)
+    const mappedTeamsWithMatches = matches.map((currentMatch) => {
+        const aTeam = teams.find((el) => el.ID === currentMatch.ATeamID)
+        const bTeam = teams.find((el) => el.ID === currentMatch.BTeamID)
+        
+        return {
+            ...currentMatch,
+            ATeamName: aTeam?.Name,
+            BTeamName: bTeam?.Name
+        }
+    })
+
+    console.log(mappedTeamsWithMatches)
+    
     let values = {
-        x:5
+        x: 5
     }
+
+
 
     return (
         <TournamentContext.Provider value={values}>
