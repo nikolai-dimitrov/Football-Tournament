7df75b8fbbb8f87635fd9882fec32c288bb38025
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-04 15:42:38 +0300

    feat: create home page group stage ui

--- Football-Tournament/src/components/Home/Home.jsx
+++ Football-Tournament/src/components/Home/Home.jsx
@@ -1,23 +1,46 @@
 import { useContext } from "react"
 import { TournamentContext } from "../../contexts/TournamentContext"
+
+import styles from './home.module.css'
+
 export const Home = () => {
     const { matchesGroupStageSchema, matchesPlayedAfterGroups, teams } = useContext(TournamentContext);
-    // console.log(Object.entries(matchesGroupStageSchema).sort((a, b) => a[0].localeCompare(b[0])))
     console.log(matchesGroupStageSchema)
+
     return (
         <div>
-            {matchesGroupStageSchema?.map(([matchGroupName, groupMatchesArray], index) => {
-                return (
-                    <div key={index}>
-                        {matchGroupName}
-                        {groupMatchesArray.map((currentMatch) => {
-                            return (
-                                <p key={currentMatch.ID}> {currentMatch.ID}, {currentMatch.ATeamName}, {currentMatch.BTeamName} , {currentMatch.Score} , {currentMatch.Date} </p>
-                            )
-                        })}
-                    </div>
-                )
-            })}
+            <section className={styles.groupCardsContainer}>
+                {matchesGroupStageSchema?.map(([matchGroupName, groupMatchesArray], index) => {
+                    return (
+                        <div className={styles.groupContainer} key={index}>
+                            <h3>{matchGroupName}</h3>
+                            <ul className={styles.groupContent}>
+                                {groupMatchesArray.map((currentMatch, index) => {
+                                    return (
+                                        <li className={styles.matchContainer} key={currentMatch.ID}>
+                                            <p>{currentMatch.Date}</p>
+                                            <div className={styles.scoreInformationContainer}>
+                                                <div className={currentMatch.teamWinner == currentMatch.ATeamName ? styles.winnerHighlight : '' }>
+                                                    <p>{currentMatch.ATeamName}</p>
+                                                    <p>{currentMatch.teamAscore}</p>
+                                                </div>
+                                                <div className={currentMatch.teamWinner == currentMatch.BTeamName ? styles.winnerHighlight : '' }>
+                                                    <p>{currentMatch.BTeamName}</p>
+                                                    <p>{currentMatch.teamBscore}</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    )
+                })}
+            </section>
+            <section>
+                {/* Qualified teams schema section */}
+            </section>
         </div>
+
     )
 }
