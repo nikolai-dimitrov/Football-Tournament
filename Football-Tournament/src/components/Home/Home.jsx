45f5b84637500e013d84d6f56ba13fb0a65b9de2
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-04 13:19:25 +0300

    feat: implement functionality to display group with its matches in plain html

--- Football-Tournament/src/components/Home/Home.jsx
+++ Football-Tournament/src/components/Home/Home.jsx
@@ -1,5 +1,22 @@
+import { useContext } from "react"
+import { TournamentContext } from "../../contexts/TournamentContext"
 export const Home = () => {
+    const { matchesGroupStageSchema, matchesPlayedAfterGroups, teams } = useContext(TournamentContext);
+
     return (
-        <div>Home</div>
+        <div>
+            {Object.entries(matchesGroupStageSchema)?.map(([matchGroupName, groupMatchesArray], index) => {
+                return (
+                    <div key={index}>
+                        {matchGroupName}
+                        {groupMatchesArray.map((currentMatch) => {
+                            return (
+                                <p key={currentMatch.ID}> {currentMatch.ID}, {currentMatch.ATeamName}, {currentMatch.BTeamName} , {currentMatch.Score} , {currentMatch.Date} </p>
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </div>
     )
 }
