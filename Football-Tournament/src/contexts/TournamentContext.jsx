cc2cec83303f6471022af603cc0d8178d8b46047
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-04 15:48:06 +0300

    refactor: add team winner property

--- Football-Tournament/src/contexts/TournamentContext.jsx
+++ Football-Tournament/src/contexts/TournamentContext.jsx
@@ -28,27 +28,38 @@ export const TournamentProvider = ({ children }) => {
             // Iterate all matches and set ATeamName = Team Name and same operation for BTeamName = Team Name and set a group where match is played
             matchesData.forEach((currentMatch) => {
 
+                const ATeamName = tempTeamsObject[currentMatch.ATeamID][0];
+                const BTeamName = tempTeamsObject[currentMatch.BTeamID][0];
+
                 const groupStageFinalDate = new Date('6/26/2024');
                 const currentMatchDate = new Date(currentMatch.Date);
                 // The group where match is played in (a team and b team are always in same group during the group stage)
                 const currentMatchGroup = tempTeamsObject[currentMatch.ATeamID][1];
                 const currentMatchScore = currentMatch.Score;
 
-                const [teamAscore, teamBscore ] = currentMatchScore.split('-');
+                const [teamAscore, teamBscore] = currentMatchScore.split('-');
+                let teamWinner = null;
 
-                console.log(teamAscore)
+                if (Number(teamAscore) > Number(teamBscore)) {
+                    teamWinner = ATeamName;
+                }
+                else if (Number(teamAscore) < Number(teamBscore)) {
+                    teamWinner = BTeamName;
+                }
+
                 const tempExtendedMatchObject = {
                     ...currentMatch,
-                    ATeamName: tempTeamsObject[currentMatch.ATeamID][0],
-                    BTeamName: tempTeamsObject[currentMatch.BTeamID][0],
+                    ATeamName,
+                    BTeamName,
                     teamAscore,
-                    teamBscore
+                    teamBscore,
+                    teamWinner
 
                 }
 
                 // Get only matches which are played in groups stage
                 if (currentMatchDate <= groupStageFinalDate) {
-                    if(!tempMatchesGroupStageSchema[currentMatchGroup]) {
+                    if (!tempMatchesGroupStageSchema[currentMatchGroup]) {
                         tempMatchesGroupStageSchema[currentMatchGroup] = [];
                     }
                     tempMatchesGroupStageSchema[currentMatchGroup].push(tempExtendedMatchObject);
