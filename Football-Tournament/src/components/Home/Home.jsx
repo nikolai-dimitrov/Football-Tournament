1e096656ff80c7393cb51abe6ae4e2c564ffe894
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-08 19:19:33 +0300

    feat: implement bracket view ui

--- Football-Tournament/src/components/Home/Home.jsx
+++ Football-Tournament/src/components/Home/Home.jsx
@@ -7,6 +7,13 @@ import styles from './home.module.css'
 export const Home = () => {
     const { matchesGroupStageSchema, matchesPlayedAfterGroupsSchema } = useContext(TournamentContext);
 
+    const classNamesMapper = {
+        0: 'roundOfEight',
+        1: 'quarterFinals',
+        2: 'semiFinals',
+        3: 'finals'
+    }
+
     return (
         <div className={styles.home}>
             <section className={styles.groupCardsContainer}>
@@ -48,15 +55,16 @@ export const Home = () => {
 
                     return (
                         <div key={index} className={styles.qualificationMatchesContent}>
-                            <h3>{['Round of 16', 'Quarter-Finals', 'Semi-Finals', 'Finals'][index]}</h3>
-                            <ul>
-                                {matchesArray.map((currentMatch) => {
+                            <h3>{['Round of Eight', 'Quarter-Finals', 'Semi-Finals', 'Finals'][index]}</h3>
+                            <ul className={styles[classNamesMapper[index]]}>
+                                {matchesArray.map((currentMatch, matchIndex) => {
                                     return (
                                         <Link
                                             key={currentMatch.ID}
                                             to={`/match/details/${currentMatch.ID}`}
                                         >
-                                            <li className={styles.matchBracket}>
+                                            <li className={matchIndex % 2 === 1 ? `${styles.matchBracket} ${styles.even}` : `${styles.matchBracket}`}>
+                                                <div className={styles.connector}></div>
                                                 <p>{currentMatch.Date}</p>
                                                 <div className={styles.scoreInformationContainer}>
                                                     <div className={currentMatch.teamWinner == currentMatch.ATeamName ? styles.winnerHighlight : ''}>
@@ -76,8 +84,8 @@ export const Home = () => {
                         </div>
                     )
                 })}
-            </section>
-        </div>
+            </section >
+        </div >
 
     )
 }
