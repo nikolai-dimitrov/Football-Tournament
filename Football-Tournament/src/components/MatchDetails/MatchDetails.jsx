df9d2df276019472bbaae727369ff3bb8c097518
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-11 15:12:29 +0300

    feat: implement separating teams and mapping players to their positions functionality
    Getting all players in match than separate them into two teams and map every player to its positions.
    
    Mapped players with positions are object with key teamId value is object with keys positions names and values of this object are arrays with players who play in corresponding position

--- Football-Tournament/src/components/MatchDetails/MatchDetails.jsx
+++ Football-Tournament/src/components/MatchDetails/MatchDetails.jsx
@@ -7,32 +7,74 @@ import styles from "./match-details.module.css"
 export const MatchDetails = () => {
     // const [playersRecords, setPlayersRecords] = [];
     const [currentMatch, setCurrentMatch] = useState({});
-    // const [playersInCurrentMatch, setPlayersInCurrentMatch] = useState({});
+    const [teamsAndPositionsSchema, setTeamsAndPositionsSchema] = useState({});
     const { matches, playersMappedWithMatches } = useContext(TournamentContext);
     const { id } = useParams();
-    console.log(id)
+
     useEffect(() => {
         const tempCurrentMatch = matches.find((el) => el.ID == id);
-        const allPlayersInMatch = playersMappedWithMatches[id]
         const { ATeamID, BTeamID } = tempCurrentMatch;
 
-        const playersFromAteam = allPlayersInMatch.filter((playerRecord) => playerRecord['playerDetails'].TeamID === ATeamID);
-        const playersFromBteam = allPlayersInMatch.filter((playerRecord) => playerRecord['playerDetails'].TeamID === BTeamID);
+        const allPlayersInMatch = playersMappedWithMatches[id];
 
+        const tempTeamsAndPositionsSchema = {
+            [ATeamID]: {},
+            [BTeamID]: {}
+        };
 
-        console.log(playersFromAteam, 'a team');
-        console.log(playersFromBteam, 'a team');
+        allPlayersInMatch.forEach((currentPlayer) => {
+            const currentPlayerPosition = currentPlayer.playerDetails.Position;
+            const currentPlayerTeamId = currentPlayer.playerDetails.TeamID;
 
+            // For each position create key DF, MF, CF and value array which contains players who play in that position
+            if (!(currentPlayerPosition in tempTeamsAndPositionsSchema[currentPlayerTeamId])) {
+                tempTeamsAndPositionsSchema[currentPlayerTeamId][currentPlayerPosition] = [];
+            }
 
-        setCurrentMatch(oldState => tempCurrentMatch);
+            tempTeamsAndPositionsSchema[currentPlayerTeamId][currentPlayerPosition].push(currentPlayer);
+
+        })
+
+        console.log(tempTeamsAndPositionsSchema, 'players in the match with their positions')
+
+        setCurrentMatch(state => tempCurrentMatch);
+        setTeamsAndPositionsSchema(state => tempTeamsAndPositionsSchema);
+        console.log(Object.entries(teamsAndPositionsSchema))
 
     }, [id])
 
     return (
         <section className={styles.matchDetails}>
-            <h1>Match Details</h1>
-            <div></div>
-            <h2>Players:</h2>
+            <>
+                <h1>Match Details</h1>
+                <div className={styles.fieldsContainer}>
+                    {Object.entries(teamsAndPositionsSchema).map(([teamID, positionObject]) => {
+                        return (
+                            <div className={styles.field}>
+                                {Object.entries(positionObject).map(([positionName, playersArray]) => {
+                                    return (
+                                        <div>
+                                            {playersArray.map((currentPlayer) => {
+                                                return (
+                                                    <>
+                                                        {
+                                                            currentPlayer.fromMinutes == '0' &&
+                                                            <div>
+                                                                <div>{currentPlayer.playerDetails.Position}</div>
+                                                            </div>
+
+                                                        }
+                                                    </>
+                                                )
+                                            })}
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                </div>
+            </>
         </section>
     )
 }
