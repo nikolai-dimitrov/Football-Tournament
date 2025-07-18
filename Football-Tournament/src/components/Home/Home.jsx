e41bbaa20c27aa0fa3a982f11d33374f982ff6c2
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-07-18 13:09:59 +0300

    feat: implement home page fade in animation

--- Football-Tournament/src/components/Home/Home.jsx
+++ Football-Tournament/src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { Link } from "react-router"
-import { countryFlags } from "../../constants/countryFlags"
+import { AnimatePresence, motion } from "framer-motion"
 import { TournamentContext } from "../../contexts/TournamentContext"
 
 import { Match } from "./Match/Match"
@@ -18,53 +18,75 @@ export const Home = () => {
     }
 
     return (
-        <div className={styles.home}>
-            <section className={styles.groupCardsContainer}>
-                <h2>Groups</h2>
-                {matchesGroupStageSchema?.map(([matchGroupName, groupMatchesArray], index) => {
-                    return (
-                        <div className={styles.groupContainer} key={index}>
-                            <h3>{matchGroupName}</h3>
-                            <ul className={styles.groupContent}>
-                                {groupMatchesArray.map((currentMatch, index) => {
-                                    return (
+        <AnimatePresence>
+            <motion.div
+                transition={{
+                    duration: 0.3,
+                    ease: "easeInOut",
+                }}
+
+                initial={{
+                    opacity: 0,
+
+                }}
+
+                animate={{
+                    opacity: 1,
+                }}
+
+                exit={{
+                    opacity: 0,
+                }}
+            >
+                <div className={styles.home}>
+                    <section className={styles.groupCardsContainer}>
+                        <h2>Groups</h2>
+                        {matchesGroupStageSchema?.map(([matchGroupName, groupMatchesArray], index) => {
+                            return (
+                                <div className={styles.groupContainer} key={index}>
+                                    <h3>{matchGroupName}</h3>
+                                    <ul className={styles.groupContent}>
+                                        {groupMatchesArray.map((currentMatch, index) => {
+                                            return (
+                                                <Link
+                                                    key={currentMatch.ID}
+                                                    to={`/match/details/${currentMatch.ID}`}
+                                                >
+                                                    <li className={styles.matchContainer}>
+                                                        <Match currentMatch={currentMatch} />
+                                                    </li>
+                                                </Link>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+                            )
+                        })}
+                    </section>
+                    <section className={styles.matchBracketsContainer}>
+                        {matchesPlayedAfterGroupsSchema.map((matchesArray, index) => (
+                            <div key={index} className={styles.qualificationMatchesContent}>
+                                <h3>{['Round of Eight', 'Quarter-Finals', 'Semi-Finals', 'Finals'][index]}</h3>
+                                <ul className={styles[classNamesMapper[index]]}>
+                                    {matchesArray.map((currentMatch, matchIndex) => (
                                         <Link
                                             key={currentMatch.ID}
                                             to={`/match/details/${currentMatch.ID}`}
                                         >
-                                            <li className={styles.matchContainer}>
+                                            <li className={matchIndex % 2 === 1 ? `${styles.matchBracket} ${styles.even}` : `${styles.matchBracket}`}>
+                                                <div className={styles.connector}></div>
                                                 <Match currentMatch={currentMatch} />
                                             </li>
                                         </Link>
-                                    )
-                                })}
-                            </ul>
-                        </div>
-                    )
-                })}
-            </section>
-            <section className={styles.matchBracketsContainer}>
-                {matchesPlayedAfterGroupsSchema.map((matchesArray, index) => (
-                    <div key={index} className={styles.qualificationMatchesContent}>
-                        <h3>{['Round of Eight', 'Quarter-Finals', 'Semi-Finals', 'Finals'][index]}</h3>
-                        <ul className={styles[classNamesMapper[index]]}>
-                            {matchesArray.map((currentMatch, matchIndex) => (
-                                <Link
-                                    key={currentMatch.ID}
-                                    to={`/match/details/${currentMatch.ID}`}
-                                >
-                                    <li className={matchIndex % 2 === 1 ? `${styles.matchBracket} ${styles.even}` : `${styles.matchBracket}`}>
-                                        <div className={styles.connector}></div>
-                                        <Match currentMatch={currentMatch} />
-                                    </li>
-                                </Link>
-                            ))}
-                        </ul>
-                    </div>
-                )
-                )}
-            </section >
-        </div >
+                                    ))}
+                                </ul>
+                            </div>
+                        )
+                        )}
+                    </section >
+                </div >
+            </motion.div>
+        </AnimatePresence>
 
     )
 }
